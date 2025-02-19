60272ac245adb04bebbb55297e2ac1ca454ec7be
Ghady04 <168630743+Ghady04@users.noreply.github.com>
2025-02-19 20:48:38 +0400

    Delete security_tests directory

--- security_tests/mitm_sniffer.py
+++
@@ -1,9 +0,0 @@
-from scapy.all import sniff
-
-def packet_callback(packet):
-    if packet.haslayer("Raw"):
-        data = packet["Raw"].load
-        if b"Authorization" in data or b"password" in data:
-            print(f"⚠️ Possible Credentials Leak: {data}")
-
-sniff(filter="tcp port 80", prn=packet_callback, store=0)
